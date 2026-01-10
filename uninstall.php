403bc649264ad1f8e7589d85639cf95549319a48
peanutsx50 <murimifestus09@gmail.com>
2026-01-10 12:26:04 +0300

    updated uninstall function

--- uninstall.php
+++ uninstall.php
@@ -6,17 +6,33 @@ if (! defined('WP_UNINSTALL_PLUGIN')) {
 }
 
 // Remove custom post type on uninstall
-$books = get_posts(array(
-    'post_type' => 'book',
+$posts = get_posts(array(
+    'post_type' => 'mpesa',
     'numberposts' => -1,
     'post_status' => 'any'
 ));
     
-foreach ($books as $book) {
-    wp_delete_post($book->ID, true); // get ID and delete permanently
+foreach ($posts as $post) {
+    wp_delete_post($post->ID, true); // get ID and delete permanently
 }
 
-// use wpdb to remove any custom tables if created
-// global $wpdb;
-// $table_name = $wpdb->prefix . 'your_custom_table';
-// $wpdb->query("DROP TABLE IF EXISTS $table_name");
+// 2. Delete all plugin settings/options
+$settings_keys = [
+    'bpmpesa_allow_payments',
+    'bpmpesa_save_transactions',
+    'bpmg_consumer_key',
+    'bpmg_consumer_secret',
+    'bpmg_shortcode',
+    'bpmg_passkey',
+    'bpmpesa_account_reference',
+    'bpmpesa_transaction_reference',
+    'bpmpesa_show_paybill',
+    'bpmpesa_payment_type',
+    'bpmpesa_paybill',
+    'bpmpesa_account',
+    'bpmpesa_amount'
+];
+
+foreach ($settings_keys as $key) {
+    delete_option($key); // if option dosent exist it returns false
+}
